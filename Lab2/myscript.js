fff9e4cc9bc91cfbd39da7f5e5ca3f62cbee6b6e
dn238608 <daniel.j.norris@me.com>
2018-09-07 13:39:41 -0400

    Adding links to footer via button.

--- Lab2/myscript.js
+++ Lab2/myscript.js
@@ -1,24 +1,28 @@
-//var newsSources = ["http://www.foxnews.com/", "http://thefederalist.com/", "https://twitchy.com/"];
-
-
-function firstElement()
+function FirstElement()
 {
-var x = document.getElementById("firstElement");
+var x = document.getElementById("FirstElement");
 x.style.color = "blue";
 }
 
-function secondElement()
+function SecondElement()
 {
-var x = document.getElementById("secondElement")
+var x = document.getElementById("SecondElement")
 x.innerHTML = "This text was set from a script";
 }
 
-function thirdElement()
+function ThirdElement()
 {
-var x = document.getElementById("thirdElement")
+var x = document.getElementById("ThirdElement")
 x.style.fontFamily = "TimesNewRoman";
 x.style.fontSize = "larger";
 }
 
+function Footer(){   
+var newsSources = [
+    "http://www.foxnews.com/", 
+    "http://thefederalist.com/", 
+    "https://twitchy.com/"];
 
-
+var x = document.getElementById("Footer")
+x.innerHTML = newsSources;
+}
