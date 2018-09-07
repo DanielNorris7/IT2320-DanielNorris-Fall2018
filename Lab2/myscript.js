71dd7d5d43d0145c5c6c8616e6ca29f6946958f0
dn238608 <daniel.j.norris@me.com>
2018-09-07 15:49:24 -0400

    Added name function

--- Lab2/myscript.js
+++ Lab2/myscript.js
@@ -1,28 +1,38 @@
 function FirstElement()
 {
-var x = document.getElementById("FirstElement");
-x.style.color = "blue";
+    var x = document.getElementById("FirstElement");
+        x.style.color = "blue";
 }
 
 function SecondElement()
 {
-var x = document.getElementById("SecondElement")
-x.innerHTML = "This text was set from a script";
+    var x = document.getElementById("SecondElement")
+        x.innerHTML = "This text was set from a script";
 }
 
 function ThirdElement()
 {
-var x = document.getElementById("ThirdElement")
-x.style.fontFamily = "TimesNewRoman";
-x.style.fontSize = "larger";
+    var x = document.getElementById("ThirdElement")
+        x.style.fontFamily = "TimesNewRoman";
+        x.style.fontSize = "larger";
 }
 
 function Footer(){   
-var newsSources = [
-    "http://www.foxnews.com/", 
-    "http://thefederalist.com/", 
-    "https://twitchy.com/"];
+    var newsSources = [
+        "http://www.foxnews.com/", 
+        "http://thefederalist.com/", 
+        "https://twitchy.com/"];
 
-var x = document.getElementById("Footer")
-x.innerHTML = newsSources;
+    var x = document.getElementById("Footer")
+        x.innerHTML = newsSources;
+}
+
+function Name()
+{
+    var first = document.getElementById("FirstName").value;
+    var last = document.getElementById("LastName").value;
+
+    var name = "Hi, "+ first + " " + last + "!";
+
+    document.getElementById("FullName").innerHTML = name;
 }
