683bc6bf574bd9f2bfa798d4678792b4d3ae1a77
dn238608 <daniel.j.norris@me.com>
2018-09-07 13:27:04 -0400

    Updating elements

---
+++ Lab2/myscript.js
@@ -0,0 +1,24 @@
+//var newsSources = ["http://www.foxnews.com/", "http://thefederalist.com/", "https://twitchy.com/"];
+
+
+function firstElement()
+{
+var x = document.getElementById("firstElement");
+x.style.color = "blue";
+}
+
+function secondElement()
+{
+var x = document.getElementById("secondElement")
+x.innerHTML = "This text was set from a script";
+}
+
+function thirdElement()
+{
+var x = document.getElementById("thirdElement")
+x.style.fontFamily = "TimesNewRoman";
+x.style.fontSize = "larger";
+}
+
+
+
