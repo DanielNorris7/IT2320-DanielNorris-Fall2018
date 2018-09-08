8d1a373602a124f53476e23e5a44c9f1048b16a2
dn238608 <daniel.j.norris@me.com>
2018-09-08 10:17:41 -0400

    All elments required for assingment

--- Lab2/myscript.js
+++ Lab2/myscript.js
@@ -1,38 +1,43 @@
-function FirstElement()
-{
+function Body() {
+    var x = document.getElementById("Body");
+    x.style.backgroundColor = "#00cc66";
+}
+
+function FirstElement() {
     var x = document.getElementById("FirstElement");
-        x.style.color = "blue";
+    x.style.color = "blue";
 }
 
-function SecondElement()
-{
+function SecondElement() {
     var x = document.getElementById("SecondElement")
-        x.innerHTML = "This text was set from a script";
+    x.innerHTML = "This text was set from a script";
 }
 
-function ThirdElement()
-{
+function ThirdElement() {
     var x = document.getElementById("ThirdElement")
-        x.style.fontFamily = "TimesNewRoman";
-        x.style.fontSize = "larger";
+    x.style.fontFamily = "TimesNewRoman";
+    x.style.fontSize = "larger";
 }
 
-function Footer(){   
-    var newsSources = [
-        "http://www.foxnews.com/", 
-        "http://thefederalist.com/", 
-        "https://twitchy.com/"];
-
-    var x = document.getElementById("Footer")
-        x.innerHTML = newsSources;
-}
-
-function Name()
-{
+function Name() {
     var first = document.getElementById("FirstName").value;
     var last = document.getElementById("LastName").value;
 
-    var name = "Hi, "+ first + " " + last + "!";
+    var name = "Hi, " + first + " " + last + "!";
 
     document.getElementById("FullName").innerHTML = name;
+}
+
+function Reload(){
+    location.reload();
+}
+
+function Footer() {
+    var newsSources = [
+        "http://www.foxnews.com/",
+        "http://thefederalist.com/",
+        "https://twitchy.com/"];
+
+    var x = document.getElementById("Footer")
+    x.innerHTML = newsSources;
 }
