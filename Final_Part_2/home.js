fa3b2fd010776193e0f71c72c214b49925a1576c
Daniel NorrisDani <Daniel.Norris@hyland.com>
2018-12-09 20:27:02 -0500

    Final - Part 2 (shelter.listByBreed)

--- Final_Part_2/home.js
+++ Final_Part_2/home.js
@@ -85,12 +85,14 @@ $(function(){
            {              
               console.log(data);            
               $("ul").remove();
-              var ul = $("<ul>Pets</ul>");
+              var ul = $("<ul>Pets!</ul>");
               $("#petResults").after(ul);
               
               $.each(data.petfinder.pets.pet,function(i,pet)
               {                  
-                    var li = $("<li></li>").text(pet.name.$t).text(pet.description.$t);
+                    var li = $("<li></li>").text(pet.name.$t)
+                    .append(pet.description.$t)
+                    .append(pet.media.photo);
                     $("ul").append(li);
               });                      
             }                     
@@ -126,7 +128,9 @@ $(function(){
               
               $.each(data.petfinder.shelters.shelter,function(i,shelter)
               {                  
-                    var li = $("<li></li>").text(shelter.city.$t).text(shelter.name.$t);
+                    var li = $("<li></li>").text(shelter.city.$t).append(": ")
+                    .append(shelter.name.$t)
+                    .append('(').append(shelter.id.$t).append(')');
                     $("ul").append(li);
               });                      
             }                     
@@ -134,4 +138,117 @@ $(function(){
          });//ajax  
     });  //btn click
    
+    $("#findPetInShelterBtn").on("click", function(){        
+        const apiKey = "551b527add03634ac3a95a7a967367e6";
+
+        // Values will only be added to the array if the value is NOT ""
+        // Keeps unneed parameters out of the query URL
+        var queryParam = [];
+
+        $("#localShelterId").val() != "" ? queryParam.push("&id="+$("#localShelterId").val()) : ""; 
+
+        console.log(queryParam.values);
+
+        var url = "http://api.petfinder.com/shelter.getPets?format=json&key="+apiKey+"&callback=?"+queryParam.join("")+"&format=json";
+       
+        $.ajax({
+           url: url,           
+           type: "GET", 
+           dataType: "jsonp",   //needed for CORS       
+           contentType: "application/json; charset=utf-8",
+           crossDomain: true,                   
+           success: function(data)
+           {              
+              console.log(data);            
+              $("ul").remove();
+              var ul = $("<ul>Local Pets:</ul>");
+              $("#localPet").after(ul);
+              
+                // Testing Shelter Id's
+                // OH1073
+                // OH290
+
+              $.each(data.petfinder.pets.pet,function(i,pet)
+              {                  
+                    var li = $("<li></li>").text(pet.animal.$t).append(": ")
+                    .append(pet.sex.$t).append(": ")
+                    .append(pet.name.$t).append(": ")
+                    .append(pet.description.$t)
+                    .append(pet.media.photo);
+                    $("ul").append(li);
+              });                      
+            }                     
+         });//ajax  
+    });  //btn click
+
+    $("#breedListBtn").on("click", function(){        
+        const apiKey = "551b527add03634ac3a95a7a967367e6";
+
+        // Values will only be added to the array if the value is NOT ""
+        // Keeps unneed parameters out of the query URL
+        var queryParam = [];
+
+        $("#animalBreed").val() != "" ? queryParam.push("&animal="+$("#animalBreed").val()) : ""; 
+
+        console.log(queryParam.values);
+
+        var url = "http://api.petfinder.com/breed.list?format=json&key="+apiKey+"&callback=?"+queryParam.join("")+"&format=json";
+       
+        $.ajax({
+           url: url,           
+           type: "GET", 
+           dataType: "jsonp",   //needed for CORS       
+           contentType: "application/json; charset=utf-8",
+           crossDomain: true,                   
+           success: function(data)
+           {              
+              console.log(data);            
+              $("ul").remove();
+              var ul = $("<ul>Pet Breeds:</ul>");
+              $("#breedResults").after(ul);
+
+              $.each(data.petfinder.breeds.breed,function(i,breed)
+              {                  
+                    var li = $("<li></li>").text(breed.$t);
+                    $("ul").append(li);
+              });                      
+            }                     
+         });//ajax  
+    });  //btn click
+
+    $("#findListOfBreedsBtn").on("click", function(){        
+        const apiKey = "551b527add03634ac3a95a7a967367e6";
+
+        // Values will only be added to the array if the value is NOT ""
+        // Keeps unneed parameters out of the query URL
+        var queryParam = [];
+
+        $("#animalBreed").val() != "" ? queryParam.push("&animal="+$("#animalBreed").val()) : ""; 
+        $("#findByBreed").val() != "" ? queryParam.push("&breed="+$("#animalBrefindByBreeded").val()) : ""; 
+
+        console.log(queryParam.values);
+
+        var url = "http://api.petfinder.com/shelter.listByBreed?format=json&key="+apiKey+"&callback=?"+queryParam.join("")+"&format=json";
+       
+        $.ajax({
+           url: url,           
+           type: "GET", 
+           dataType: "jsonp",   //needed for CORS       
+           contentType: "application/json; charset=utf-8",
+           crossDomain: true,                   
+           success: function(data)
+           {              
+              console.log(data);            
+              $("ul").remove();
+              var ul = $("<ul>Fur Babies:</ul>");
+              $("#listOfBreedResults").after(ul);
+
+              $.each(data.petfinder.shelters.shelter,function(i,shelter)
+              {                  
+                    var li = $("<li></li>").text(shelter.name.$t);
+                    $("ul").append(li);
+              });                      
+            }                     
+         });//ajax  
+    });  //btn click
 })
